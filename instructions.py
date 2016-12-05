56d1eea50aa1b778c92ba9ae93556626c3974832
Käthe <kaethe.vrettos@gmx.de>
2016-12-05 11:17:30 +0100

    created instructions.py
    Kann am Frontend ausgegeben werden z.B. bei /help
    Spielablauf ist noch nicht geschrieben

---
+++ instructions.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# einführender Text:
+a1 = "Das kleine Dorf Düsterwald kennt nur noch Angst und Schrecken. "
+a2 = "Nachts, wenn die Dunkelheit gegen das Licht gewinnt und kein Mensch sich wohlfühlt, suchen mörderische Werwölfe das Dorf Düsterwald heim und entführen jede Nacht einen Dorfbewohner. "
+a3 = "Niemand kehrt je lebend zurück, aber eines ist gewiss – der Feind weilt direkt unter uns. "
+a4 = "In diesem kommunikativen Kartenspiel müssen die Spieler durch geschicktes Kombinieren und logischen Denken herausfinden, welche der Dorfbewohner Werwölfe sind und diese am Tag hinrichten. "
+a5 = "Das Spiel lebt von der Kommunikation und erlaubt viel Spielraum für Täuschungsmanöver und interessante Spielzüge. "
+a6 = "Ein tolles Gesellschaftsspiel, das man, einmal gespielt, nie mehr missen will."
+einfuehrenderText = (a1 + a2 + a3 + a4 + a5 + a6)
+
+# Spielvorbereitung
+b0 = "Bei der Telegram Version muss kein Spielleiter im originalen Sinne gewählt werden. "
+b1 = "Dieser Spieler sollte keine Probleme damit haben, sich eine Geschichte rund um das Kartenspiel auszudenken und darf jedoch keinen Überblick über die Rollenverteilung haben."
+b2 = "Die Spieler stellen sind in einem Kreis aus und nehmen dort Platz. "
+b3 = "Je nach Spieleranzahl muss geschaut werden, wie viele Sonderrollen das Spiel enthalten soll. "
+b4 = "Die optimale Anzahl an Werwölfen wird automatisch berechnet. "
+b5 = "Der Spielleiter muss also wie bei start beschrieben seinen Namen, die Spieleranzahl, die gewollten Sonderrollen und das gewünschte Passwort festlegen. "
+b6 = "Jeder spieler fordert dann über Telegram dann mit seinem Namen, dem festgelegtn Passwort und evtl dem Sitzungsnamen einen Platz im Spiel an. "
+b7 = "Sobald alle registriert sind, wird jedem Spieler seine Rolle zugeteilt. "
+b8 = "Jeder Spieler schaut sich seine eigene Karte an, zeigt aber am besten keine Regung, denn schlaue Spieler schauen schon hier auf die Reaktionen, um eventuell herauszufinden, welche Spieler böse sind. "
+b9 = "Hat jeder Spieler seine Karte angeschaut und der Spielleiter alles notiert, beginnt die erste Nachtphase."
+spielVorbereitung = (b0 + b1 + b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8 + b9)
+
+# SpielAblauf
+c01 = "Die Nachtphase"
+spielAblauf = (c01)
+
+# Ziel des Spiels
+d1 = "Die Werwölfe müssen versuchen, die Dorfbewohner gegeneinander auszuspielen und alle aus dem Spiel zu entfernen. "
+d2 = "Gibt es nach eine Tagphase eine Situation, in der es 2 Werwölfe und 2 Dorfbewohner gibt, gewinnen die Werwölfe, da sie in dieser Situation die Mehrheit im Spiel besitzen. "
+d3 = "Die Dorfbewohner müssen versuchen, alle Werwölfe am Tag zu lynchen. Die Dorfbewohner gewinnen nur, falls sie es schaffen, alle Werwölfe zu lynchen. "
+spielZiel = (d1 + d2 + d3)
+
+# Spielfiguren
+e1 = "Im Spiel gibt es jede Menge besonderer Spielfiguren, die einmalige Fähigkeiten besitzen.  "
+e2 = "Folgend möchten wir die wichtigsten Spielcharaktere erklären und wertvolle Tipps geben, wie man diese geschickt ausspielt."
+e3 = "Es gibt die folgenden Figuren im Threema Werwolf-Spiel: Amor, Seherin, Hexe und Jäger"
+spielFiguren = (e1 + e2 + e3)
+
+# Spielfiguren im Einzelnen
+Hexe = "Die Hexe erwacht, nachdem die Werwölfe sich ein Opfer ausgesucht haben. Die Hexe besitzt das Spiel über einen Gift- und einen Heiltrank. Der Spielleiter fragt die Hexe jede Nacht, ob sie einen Werwolf töten oder einen Dorfbewohner retten will. Beides ist ebenfalls in einer Nacht möglich. Die Hexe sieht, welche Person sterben soll und muss sich entscheiden, ob sie die Person retten will – dann gibt es für eine Nacht keinen Toten. Ist sich die Hexe bei einem Werwolf sicher, kann sie den Gifttrank ausspielen. Auch wenn beide Tränke verbraucht sind, sollte der Spielleiter die Hexe jede Nacht weiter fragen, damit die anderen Spieler nicht wissen, welche Tränke die Hexe noch hat – auch wenn ein aufmerksamer Spieler durch die Anzahl der Toten darauf schließen kann, ob die Hexe noch Tränke besitzt."
+Jaeger = "Wird der Jäger gelyncht oder über Nacht getötet, darf er einen Spieler mit in den Tod reißen."
+Amor = "Armor wacht ausschließlich in der ersten Nacht auf und verkuppelt zwei Spieler. Diese werden vom Spielleiter berührt und öffnen die Augen. Sie sind ab sofort ein Liebespaar und dürfen nicht gegeneinander abstimmen (auch wenn sie Dorfbewohner und Werwolf sind). Sie müssen sich gegenseitig beschützen und gewinnen das Spiel nur zusammen (außer beide gehören einer Partei an). Stirbt einer der Partner, stirbt der andere mit ihm."
+Seherin = "Die Seherin erwacht jede Nacht und erfragt, ob der ausgewählte Dorfbewohner Werwolf ist oder nicht. Das Wissen, das die Seherin über Nacht erlangt, muss jedoch vorsichtig eingesetzt werden, damit die Werwölfe ihr nicht auf die Schliche kommen."
+
+# Tipps und Tricks
+f1 = "Für die Werwölfe von Düsterwald gibt es jede Menge Tipps, die der Spieler anwenden kann, um zu gewinnen. "
+f2 = "Als Dorfbewohner sollte man die Mimik der Spieler beobachten und auch kontrollieren, wer für wen die Hand hebt. "
+f3 = "Auch können nachts Geräusche darüber Aufschluss geben, welcher Dorfbewohner ein Werwolf ist. "
+f4 = "Bringt Spieler unter Druck und beobachtet, welcher Spieler wen verteidigt, um daraus Schlüsse zu ziehen. "
+f5 = "Als Werwolf gibt es ebenso viele Varianten, um Dorfbewohner in die Irre zu führen. "
+f6 = "Die bekannteste Taktik ist es, einen Werwolf zu opfern, um bis zum Ende sicher zu sein."
+f7 = "Natürlich kann man sich auch für einen Werwolf melden in der Tagphase, falls man sieht, dass dieser keine Chance mehr hat, der Lynchung zu entgehen."
+tipps = (f1 + f2 + f3 + f4 + f5 + f6 + f7)
