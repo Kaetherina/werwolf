3c8e20e5ae14b17212fe44051df184a9ab62355b
Käthe <kaethe.vrettos@gmx.de>
2016-12-05 11:26:54 +0100

    Added BackendTests/registrationTest.py Also removed the unnecessary folder htmldocs

---
+++ BackendTests/registrationTest.py
@@ -0,0 +1,14 @@
+import registrationLogic
+
+registrationLogic.reset("Kevin")
+registrationLogic.getReady("Kevin", 5, ["Seherin"], "")
+
+
+registrationLogic.newPlayer("Kaethe", "werwolf")
+print("entered new player")
+registrationLogic.newPlayer("Lucas", "werwolf")
+print("entered new player")
+registrationLogic.newPlayer("Leander", "werwolf")
+print("entered new player")
+registrationLogic.newPlayer("Johannes", "werwolf")
+print("entered new player")
