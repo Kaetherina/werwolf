0088c33c756b3c320c2f2fd740786d65e33538c1
Kaethe <kaethe.vrettos@hpe.com>
2016-12-07 14:38:15 +0100

    updated bot2 and registrationLogic for easier registration!

--- bot2.py
+++ bot2.py
@@ -9,83 +9,79 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 #dispatcher = updater.dispatcher
 
 
-
+gameReady = False
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
-
-numOfplayers = 0
-wantedSpecialRoles = []
-password = None
-name = None
-specialRoles = ["Amor", "Seherin", "Hexe", "Jaeger"]
-
-
 def start(bot, update):
-    bot.sendMessage(chat_id = update.message.chat_id, text ="Bitte teile mir deine Daten mit folgenden Commands mit!")
-    bot.sendMessage(chat_id = update.message.chat_id, text ="Deinen Namen mit /myname NAME")
+    bot.sendMessage(chat_id = update.message.chat_id, text ="Hi, das hier ist unser Werwolf Spiel!")
+    bot.sendMessage(chat_id = update.message.chat_id, text ="Wenn du der Spielmaster bist, gib jetzt /master ein, sonst /spieler")
+
+def master(bot, update, args):
+    bot.sendMessage(chat_id = update.message.chat_id, text ="Perfekt, direkt beim Spielmaster! Bitte gibt uns mit den folgenden Commands wichtige Infos:")
+    bot.sendMessage(chat_id = update.message.chat_id, text ="Deinen Namen mit /mastername NAME")
     bot.sendMessage(chat_id = update.message.chat_id, text ="Anzahl der Spieler mit /anzahl ZAHL")
-    bot.sendMessage(chat_id = update.message.chat_id, text ="Die Wahl deiner 2 Klassen mit /class KLASSE1 KLASSE2")
+    bot.sendMessage(chat_id = update.message.chat_id, text ="Die Wahl so vieler Klassen wie du möchtest mit /class KLASSE1 KLASSE2")
     bot.sendMessage(chat_id = update.message.chat_id, text ="Fuer mehr Infos zu den Klassen, benutze /classhelp")
     bot.sendMessage(chat_id = update.message.chat_id, text ="Das Passwort fuer dein Spiel mit /pw PASSWORT")
+    bot.sendMessage(chat_id = update.message.chat_id, text ="..alternativ kannst du dieses Kommando auslassen und das Passwort bleibt \"werwolf\"")    
     bot.sendMessage(chat_id = update.message.chat_id, text ="Sobald du alles eingegeben hast, schreibe bitte /ready")
     bot.sendMessage(chat_id = update.message.chat_id, text ="P.S: Solltest du dich mal vertippt haben, so gebe einen Command einfach erneut ein! :)")
-    
 
-# Create the Updater and pass it your bot's token.
-
-def set_name(bot, update, args):
-    global name
-    name = str(args[0])
-    bot.sendMessage(chat_id = update.message.chat_id, text = "Vielen Dank " + name)
-
-def set_anzahl(bot, update, args):  
-    global numOfplayers
-    numOfplayers = int(args[0])
-    bot.sendMessage(chat_id = update.message.chat_id, text="Ok, du spielst mit " + str(args[0]) +" Spielern")
-
+def set_master_name(bot, update, args):
+    registrationLogic.setMaster(str(args[0]))
+    bot.sendMessage(chat_id = update.message.chat_id, text = "Vielen Dank " + name +", du bist der Spielmaster ")
+def set_anzahl(bot, update, args):
+    num = int(args[0]) 
+    if num<4:
+        bot.sendMessage(chat_id = update.message.chat_id, text="Es können nur 5 oder mehr mitspielen - bitte gibt die Spieleranzahl erneut ein.")
+    else:
+        registrationLogic.setNumOfPlayers(int(args[0]))
+        bot.sendMessage(chat_id = update.message.chat_id, text="Ok, du spielst mit " + num +" Spielern")
 def set_class(bot, update, args):
-    global wantedSpecialRoles
-    if args[0] != args[1]:
-        for i in specialRoles:
-            if i == args[0]:
-                wantedSpecialRoles.append(i)
-            elif i == args[1]:
-                wantedSpecialRoles.append(i)
-        bot.sendMessage(chat_id = update.message.chat_id, text="Vielen dank, deine Klassen sind: ")
-        bot.sendMessage(chat_id = update.message.chat_id, text= wantedSpecialRoles[0] + " und " + wantedSpecialRoles[1])
-    else:
+    roles = registrationLogic.setSpecialRoles(args) #gibt bei Fehler False zurück und wenn es passt das feld roles[]   
+    if bool == False:
         bot.sendMessage(chat_id = update.message.chat_id, text="Falsche Klassen! tippe /classhelp fuer mehr Infos!")
-
-def classhelp(bot, update):
-    bot.sendMessage(chat_id = update.message.chat_id, text="Es gibt folgende Klassen, aus denen du 2 wahlen musst! Achte auf die Schreibweise der Klassen!")
-    for i in specialRoles:
-        bot.sendMessage(chat_id = update.message.chat_id, text=i)
-
-def set_pw(bot, update, args):
-    global password
-    password = str(args[0])
-    print(password)
-    bot.sendMessage(chat_id = update.message.chat_id, text="Vielen Dank, dein Passwort lautet: " + password)
-
-def ready(bot, update):
-    if name == None:
-        bot.sendMessage(chat_id = update.message.chat_id, text = "Du hast keinen Namen angegeben, tippe /myname NAME")
-    elif numOfplayers <= 4:
-        bot.sendMessage(chat_id = update.message.chat_id, text = "Spielst du etwa mit weniger als fünf Leuten? Sag uns bitte mit /anzahl ZAHL , mit wie vielen Leuten du spielst! :)")
-    elif password == None:
-        bot.sendMessage(chat_id = update.message.chat_id, text = "Bitte gebe ein Passwort an. Nutze dafuer /pw PASSWORD")
     else:
-        
-        bot.sendMessage(chat_id = update.message.chat_id, text = "Vielen Dank, deine Daten wurden uebermittelt! :)")
-        registrationLogic.getReady(name, numOfplayers, wantedSpecialRoles, password)
+        bot.sendMessage(chat_id = update.message.chat_id, text="Vielen dank, die verfügbaren Klassen sind: " + str(roles))
+def set_pw(bot, update, args):
+    registrationLogic.setPassword(str(args[0]))
+    bot.sendMessage(chat_id = update.message.chat_id, text="Vielen Dank, dein Passwort lautet: " + password)
+def ready(bot, update):
+    ready = registrationLogic.ready()
+
+    if ready=="master":
+        bot.sendMessage(chat_id = update.message.chat_id, text = "Bitte gib noch deinen Namen ein mit /mastername NAME")
+    elif ready == "players":
+        bot.sendMessage(chat_id = update.message.chat_id, text = "Die Spielerzahl passt noch nicht, bitte gib eine Zahl über 4 ein mit /anzahl ZAHL")
+    elif ready == "roles":
+        bot.sendMessage(chat_id = update.message.chat_id, text = "Du hast keine Sonderrollen ausgewählt, also spielt ihr ohne Amor, Seherin, Hexe und Jaeger")
+    elif ready == "ready":
+        bot.sendMessage(chat_id = update.message.chat_id, text = "Super! Jetzt können sich die anderen Spieler anmelden, das verwendete Passwort ist "+ registrationLogic.getpw())
+        gameReady = True
+    else:
+        bot.sendMessage(chat_id = update.message.chat_id, text = "some internal failure")
+def classhelp(bot, update):
+    bot.sendMessage(chat_id = update.message.chat_id, text="Es gibt die Klassen Amor, Seherin, Hexe und Jaeger. Achte bitte auf die Schreibweise der Klassen!") 
+
+
+def spieler(bot,update,args):
+    if gameReady:
+
+    else:
+        bot.sendMessage(chat_id = update.message.chat_id, text ="Bitte warte bis euer Spielmaster alle nötigen Daten eingetragen hat!")
+        bot.sendMessage(chat_id = update.message.chat_id, text ="Gib, sobald er fertig ist nochmal /spieler ein")
+# Create the Updater and pass it your bot's token.
 
 
 updater = Updater(token = '327185418:AAGrpXhyStp8mpt2AagG18QYeTFMz0AT9PY')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('myname', set_name, pass_args = True))
+updater.dispatcher.add_handler(CommandHandler('master', master))
+updater.dispatcher.add_handler(CommandHandler('spieler', spieler))
+
+updater.dispatcher.add_handler(CommandHandler('mastername', set_master_name, pass_args = True))
 updater.dispatcher.add_handler(CommandHandler('anzahl', set_anzahl, pass_args = True))
 updater.dispatcher.add_handler(CommandHandler('class', set_class, pass_args = True))
 updater.dispatcher.add_handler(CommandHandler('classhelp', classhelp))
--- registrationLogic.py
+++ registrationLogic.py
@@ -1,61 +1,52 @@
 import time
 import random
+import logging
 
 master = "not set"
 allowedRoles = ["Amor", "Seherin", "Hexe", "Jaeger"]
 players = []
 roles = []
 playersRoles = []
+specialRoles = []
 numPlayers = 0
 pw = "werwolf"
 
-
-def getReady(name, numOfPlayers, wantedSpecialRoles, password):
-	global numPlayers 
-	numPlayers = numOfPlayers
+def setMaster(name):
+	global players
+	global master 
 	master = name
-	print("the master is "+ name)
-	if password:
-		pw = password
-		print("the password was set to "+ password)
-	else:
-		print("the password remains werwolf")
 	players.append(name)
-
-	if not testSpecialRoles(wantedSpecialRoles):
-		return False
-		print("not all special roles were found.. do some spellcheck")
+	print("the master is set to \""+name+"\"")
+def setPassword(password):
+	global password
+	pw = password
+	print("the password was set to \"" + password +"\"")
+def getPassword():
+	return pw
+def setNumOfPlayers(numOfPlayers):
+	global numPlayers
+	numPlayers = numOfPlayers
+	print("The number of players is "+ numPlayers)
+def setSpecialRoles(wantedSpecialRoles):
 	global roles
-	roles = getAllRoles(numOfPlayers, wantedSpecialRoles)
-	print("and these are all the roles: " + str(roles))
-	return True
-
-	
-def newPlayer(name, inputPassword):
-	if inputPassword==pw:
-		if name in players:
-			return "!name"
-		players.append(name)
-		print("we currently have these players: "+ str(players))
-		print("the length of the array is currently "+ str(len(players)) + "while the number of players is " + str(numPlayers))
-
-		if numPlayers==len(players):
-			giveRoles()
-			print("got all players now! Proceed to the game")
-			return "success"
-		return "player added"
+	if not(testSpecialRoles)
+		print(not all special roles were found.. do some spellcheck)
+		return False
 	else:
-		return "!password"
-
+		global specialRoles 
+		specialRoles= wantedSpecialRoles
+		roles = getAllRoles(numPlayers, wantedSpecialRoles)
+		print("These are all the possible roles: "+ str(roles))
+		return roles
 
 def testSpecialRoles(specialRoles):
+	temp = -1
 	for i in range(0, len(specialRoles)):
 		if allowedRoles.index(specialRoles[i])<0:
 			return False
 	return True
-
 def getAllRoles(num, specialRoles):
-	roles = []
+	global roles
 	if num<8:
 		numWerwolf = 1
 	elif num<13:
@@ -76,6 +67,33 @@ def getAllRoles(num, specialRoles):
 		else:
 			roles.append(specialRoles[i-numWerwolf-numBuerger])
 	return roles
+
+def ready()
+	if master == "not set":
+		return "master"
+	elif numPlayers <5:
+		return "players"
+	elif len(specialRoles) = 0:
+		return "roles"
+	else:
+		return "ready"
+
+
+def newPlayer(name, inputPassword):
+	if inputPassword==pw:
+		if name in players:
+			return "!name"
+		players.append(name)
+		print("we currently have these players: "+ str(players))
+		print("the length of the array is currently "+ str(len(players)) + "while the number of players is " + str(numPlayers))
+
+		if numPlayers==len(players):
+			giveRoles()
+			print("got all players now! Proceed to the game")
+			return "success"
+		return "player added"
+	else:
+		return "!password"
 								
 def giveRoles():
 	global playersRoles
